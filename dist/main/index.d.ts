e5f6aaf1e9624de68410d8524f40db094f570f5e
Greg Mertzlufft <greg@bikebloc.com>
2016-11-06 19:43:28 -0800

    udate for breaking changes to utter undertanding

--- dist/main/index.d.ts
+++ dist/main/index.d.ts
@@ -1,14 +1,14 @@
 import { IntentHandler } from 'utter-understanding/dist/main/handlers/intent/IntentHandler';
-import { AlexaResponse } from 'utter-understanding/dist/main/response/AlexaResponse';
+import { SpeechletResponseEnvelope } from 'utter-understanding/dist/main/response/SpeechletResponseEnvelope';
 import { RequestHandler } from 'utter-understanding/dist/main/handlers/request/RequestHandler';
 import { RequestPostProcessor } from 'utter-understanding/dist/main/handlers/request/RequestPostProcessor';
 export declare class MyPreProcessor extends RequestHandler {
-    handleRequest(event: any, context: any): Promise<AlexaResponse>;
+    handleRequest(event: any, context: any): Promise<SpeechletResponseEnvelope>;
 }
 export declare class MyAmazonHelpIntentHandler extends IntentHandler {
-    handleIntent(event: any, context: any): Promise<AlexaResponse>;
+    handleIntent(event: any, context: any): Promise<SpeechletResponseEnvelope>;
 }
 export declare class MyPostProcessHandler extends RequestPostProcessor {
-    handleRequest(event: any, context: any, response: AlexaResponse): Promise<AlexaResponse>;
+    handleRequest(event: any, context: any, response: SpeechletResponseEnvelope): Promise<SpeechletResponseEnvelope>;
 }
 export declare function handler(event: any, context: any): void;
--- dist/main/index.js
+++ dist/main/index.js
@@ -1,12 +1,12 @@
 "use strict";
-const UtterUnderstanding_1 = require('utter-understanding/dist/main/UtterUnderstanding');
-const RequestNames_1 = require('utter-understanding/dist/main/handlers/request/RequestNames');
-const IntentHandler_1 = require('utter-understanding/dist/main/handlers/intent/IntentHandler');
-const AlexaResponse_1 = require('utter-understanding/dist/main/response/AlexaResponse');
-const RequestHandler_1 = require('utter-understanding/dist/main/handlers/request/RequestHandler');
-const IntentRequestHandler_1 = require('utter-understanding/dist/main/handlers/request/IntentRequestHandler');
-const RequestPostProcessor_1 = require('utter-understanding/dist/main/handlers/request/RequestPostProcessor');
-const IntentNames_1 = require('utter-understanding/dist/main/handlers/intent/IntentNames');
+const UtterUnderstanding_1 = require("utter-understanding/dist/main/UtterUnderstanding");
+const RequestNames_1 = require("utter-understanding/dist/main/handlers/request/RequestNames");
+const IntentHandler_1 = require("utter-understanding/dist/main/handlers/intent/IntentHandler");
+const SpeechletResponseEnvelope_1 = require("utter-understanding/dist/main/response/SpeechletResponseEnvelope");
+const RequestHandler_1 = require("utter-understanding/dist/main/handlers/request/RequestHandler");
+const IntentRequestHandler_1 = require("utter-understanding/dist/main/handlers/request/IntentRequestHandler");
+const RequestPostProcessor_1 = require("utter-understanding/dist/main/handlers/request/RequestPostProcessor");
+const AmazonIntentNames_1 = require("utter-understanding/dist/main/handlers/intent/AmazonIntentNames");
 class MyPreProcessor extends RequestHandler_1.RequestHandler {
     handleRequest(event, context) {
         this.logger.debug('W00T::Preprocessing Request!');
@@ -17,7 +17,7 @@ exports.MyPreProcessor = MyPreProcessor;
 class MyAmazonHelpIntentHandler extends IntentHandler_1.IntentHandler {
     handleIntent(event, context) {
         this.logger.debug('W00T::Handling a Help Intent!');
-        return new Promise((resolve) => { resolve(AlexaResponse_1.AlexaResponse.defaultInstance); });
+        return new Promise((resolve) => { resolve(SpeechletResponseEnvelope_1.SpeechletResponseEnvelope.defaultInstance); });
     }
 }
 exports.MyAmazonHelpIntentHandler = MyAmazonHelpIntentHandler;
@@ -33,7 +33,7 @@ function handler(event, context) {
     let myHelpHandler = new MyAmazonHelpIntentHandler('MyAmazonHelpIntentHandler');
     let myRequestHandler = new IntentRequestHandler_1.IntentRequestHandler();
     let myPostProcessHandler = new MyPostProcessHandler('MyPostProcessHandler');
-    myRequestHandler.registerIntentHandler(IntentNames_1.default.Amazon.HelpIntent, myHelpHandler);
+    myRequestHandler.registerIntentHandler(AmazonIntentNames_1.default.HelpIntent, myHelpHandler);
     let utter = new UtterUnderstanding_1.UtterUnderstanding();
     utter.registerPreProcessHandler(myPreProcessor);
     utter.registerRequestHandler(RequestNames_1.default.IntentRequest, myRequestHandler);
